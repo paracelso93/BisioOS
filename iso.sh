3f7747d1a6d089995a982bd68a3273797f004f6c
paracelso93 <68126328+paracelso93@users.noreply.github.com>
2020-12-31 18:16:36 +0100

    Name
    change names to bisios

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/bisios.kernel isodir/boot/bisios.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-    multiboot /boot/myos.kernel
+menuentry "bisios" {
+    multiboot /boot/bisios.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o bisios.iso isodir
